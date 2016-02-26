9610823d37b36c1fefd7b4c08a1dfa0701449ca8
alygator3017 <aschmidt36@my.wctc.edu>
2016-02-26 10:06:16 -0600

    working with email and volunteer page- web.config is not complete with current server

--- wclc/Controllers/HomeController.cs
+++ wclc/Controllers/HomeController.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using Postal;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using wclc.Models;
+using System.Net;
+using System.Net.Mail;
 
 namespace wclc.Controllers
 {
@@ -13,18 +17,33 @@ namespace wclc.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Donate()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
-        public ActionResult Contact()
+        public ActionResult Volunteer()
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Volunteer(FormCollection values)
+        {
+            try { 
+            dynamic volEmail = new Email("VolunteerCheckedEmail");
+            volEmail.To = "alygator3017@gmail.com";
+            volEmail.Name = Request.Form["Name"];
+            volEmail.Phone = Request.Form["Phone"];
+            volEmail.Email = Request.Form["Email"];
+            volEmail.Send();
+            return View();
+            }
+            catch (Exception ex)
+            {
+                ViewBag.errorMsg = ex.GetBaseException();
+                return View();
+            }
         }
     }
 }
---
+++ wclc/Models/VolunteerForm.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace wclc.Models
+{
+    public class VolunteerForm
+    {
+        [Required]
+        [DisplayName("Name (First and Last)")]
+        [StringLength(322)]
+        public string Name { get; set; }
+        [Required]
+        [StringLength(24)]
+        [DataType(DataType.PhoneNumber)]
+        public string Phone { get; set; }
+        [Required]
+        [DisplayName("Email Address")]
+        [RegularExpression(@"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}",
+            ErrorMessage = "Email is is not valid.")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+    }
+}
